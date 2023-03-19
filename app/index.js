78704077003d0e62e60a1cb3b00ef177a438f83e
Sifan23 <sifanyg123@gmail.com>
2023-03-19 09:00:33 +0300

    the main body

--- app/index.js
+++ app/index.js
@@ -18,14 +18,26 @@ const Home = () => {
           headerStyle: { backgroundColor: COLORS.lightWhite },
           headerShadowVisible: false,
           headerLeft: () => (
-            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%"/>
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
           ),
           headerRight: () => (
-            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%"/>
+            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
           ),
-          headerTitle: ""
+          headerTitle: "",
         }}
       />
+      <ScrollView shows Vertical Scroll Indicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
